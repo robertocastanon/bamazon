574f3e67c18d06f98f165ce5d182f478319d28f3
Roberto Castanon <robertocastanon925@gmail.com>
2019-05-11 11:58:17 -0700

    npm init, inquirer and mysql added, also added some basic info to the sql file

---
+++ bamazonSeeds.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products(
+  sku INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(45) NULL,
+  department_name VARCHAR(45) NULL,
+  price DECIMAL (10,2) NULL,
+  stock_quantity INT DEFAULT 0 NULL,
+  PRIMARY KEY (sku)
+);
